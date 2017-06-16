72b749e1a964c6f902e1fa92c298e9695184cd00
Michael L Perry <michael@qedcode.com>
2017-06-15 22:25:44 -0500

    Record the team roster.

--- Source/MajorLeagueMiruken.Domain/Team.cs
+++ Source/MajorLeagueMiruken.Domain/Team.cs
@@ -1,10 +1,14 @@
-﻿using Assisticant.Fields;
+﻿using System;
+using Assisticant.Fields;
+using System.Collections.Generic;
+using Assisticant.Collections;
 
 namespace MajorLeagueMiruken.Domain
 {
     public class Team
     {
         private Observable<Person> _manager = new Observable<Person>();
+        private ObservableList<Player> _roster = new ObservableList<Player>();
 
         internal Team()
         {
@@ -14,6 +18,15 @@ namespace MajorLeagueMiruken.Domain
         {
             get => _manager;
             set => _manager.Value = value;
+        }
+
+        public IEnumerable<Player> Roster => _roster;
+
+        public Player AddPlayer(Person person)
+        {
+            var player = new Player(person, this);
+            _roster.Add(player);
+            return player;
         }
     }
 }
---
+++ Test/MajorLeagueMiruken.Domain.Test/TeamTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace MajorLeagueMiruken.Domain.Test
+{
+    [TestClass]
+    public class TeamTest
+    {
+        private League _league;
+
+        [TestInitialize]
+        public void InitializeLeague()
+        {
+            _league = new League();
+        }
+
+        [TestMethod]
+        public void CanAddPlayer()
+        {
+            var team = GivenTeam();
+            var person = GivenPerson();
+
+            var player = team.AddPlayer(person);
+
+            Assert.IsNotNull(player);
+            Assert.AreSame(person, player.Person);
+            Assert.AreSame(team, player.Team);
+        }
+
+        [TestMethod]
+        public void CanListRoster()
+        {
+            var team = GivenTeam();
+            var person = GivenPerson();
+            var player = team.AddPlayer(person);
+
+            Assert.AreEqual(1, team.Roster.Count());
+            Assert.AreSame(player, team.Roster.Single());
+        }
+
+        private Team GivenTeam()
+        {
+            return _league.CreateTeam();
+        }
+
+        private Person GivenPerson()
+        {
+            return _league.CreatePerson();
+        }
+    }
+}
