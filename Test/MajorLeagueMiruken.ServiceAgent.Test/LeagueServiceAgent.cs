5b7d1c7cb17f21eb931748274231a6dd18a5fa63
Michael L Perry <michael@qedcode.com>
2017-06-21 21:33:10 -0500

    Added service agent test.

---
+++ Test/MajorLeagueMiruken.ServiceAgent.Test/LeagueServiceAgent.cs
@@ -0,0 +1,21 @@
+﻿using MajorLeagueMiruken.Domain;
+using MajorLeagueMiruken.Mvc.Features.Team;
+using Miruken.Concurrency;
+using System;
+using System.Threading.Tasks;
+
+namespace MajorLeagueMiruken.ServiceAgent.Test
+{
+    public class LeagueServiceAgent
+    {
+        public League League { get; } = new League();
+
+        public Promise LoadTeams()
+        {
+            return P<ITeam>(IO).Teams().Then((teams, s) =>
+            {
+                Teams = teams;
+            });
+        }
+    }
+}
---
+++ Test/MajorLeagueMiruken.ServiceAgent.Test/LeagueServiceAgentTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MajorLeagueMiruken.Domain;
+using System.Linq;
+using Miruken.Concurrency;
+using System.Threading.Tasks;
+
+namespace MajorLeagueMiruken.ServiceAgent.Test
+{
+    [TestClass]
+    public class LeagueServiceAgentTest
+    {
+        [TestMethod]
+        public void InitiallyHasNoTeams()
+        {
+            var serviceAgent = GivenServiceAgent();
+            var league = serviceAgent.League;
+
+            Assert.AreEqual(0, league.Teams.Count());
+        }
+
+        [TestMethod]
+        public async Task CanLoadATeam()
+        {
+            var serviceAgent = GivenServiceAgent();
+            var league = serviceAgent.League;
+
+            Promise loading = serviceAgent.LoadTeams();
+            await loading;
+
+            Assert.AreEqual(1, league.Teams.Count());
+        }
+
+        private static LeagueServiceAgent GivenServiceAgent()
+        {
+            return new LeagueServiceAgent();
+        }
+    }
+}
