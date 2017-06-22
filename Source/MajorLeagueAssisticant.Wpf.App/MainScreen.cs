97581897ff7d670d6e7bc6b4c8355c9de30d2443
Michael L Perry <michael@qedcode.com>
2017-06-22 16:01:05 -0500

    Added Major League Assisticant

---
+++ Source/MajorLeagueAssisticant.Wpf.App/MainScreen.cs
@@ -0,0 +1,15 @@
+﻿using Assisticant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajorLeagueAssisticant.Wpf.App
+{
+    class MainScreen : ViewModelLocatorBase
+    {
+        public object MenuViewModel => null;
+        public object ContentViewModel => null;
+    }
+}
