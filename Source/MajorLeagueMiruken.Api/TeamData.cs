a038c7e16159e641f0ec2d55ac0eae7120c53bc5
Michael  Dudley <provenstyle@gmail.com>
2021-02-20 15:24:21 -0600

    formating

--- Source/MajorLeagueMiruken.Api/TeamData.cs
+++ Source/MajorLeagueMiruken.Api/TeamData.cs
@@ -1,7 +1,10 @@
 namespace MajorLeagueMiruken.Api
 {
     public record TeamData(
-        int? Id, string Name = null, Color? Color = null,
-        CoachData Coach = null, ManagerData Manager = null,
+        int?         Id, 
+        string       Name    = null, 
+        Color?       Color   = null,
+        CoachData    Coach   = null, 
+        ManagerData  Manager = null,
         PlayerData[] Players = null);
 }
