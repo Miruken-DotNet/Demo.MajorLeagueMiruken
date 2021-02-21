d368c542817b8d6712d85132b104c346e481b5cb
Michael  Dudley <provenstyle@gmail.com>
2021-02-21 13:50:52 -0600

    adding a console client

---
+++ MajorLeagueMiruken.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MajorLeagueMiruken.Client
+{
+    using System.Threading.Tasks;
+    using Api;
+    using Microsoft.Extensions.DependencyInjection;
+    using Miruken.Api;
+    using Miruken.Api.Route;
+    using Miruken.Register;
+
+    class Program
+    {
+        private const string WebHost = "https://localhost:5001/";
+
+        static async Task Main(string[] args)
+        {
+            var handler = new ServiceCollection()
+                .AddMiruken()
+                .Build();
+
+            var result = await handler.Send(new CreatePerson(
+                new PersonData(
+                    null, 
+                    "Michael", 
+                    "Dudley", 
+                    new DateTime(1977, 8, 28)
+                )).RouteTo(WebHost)
+            );
+
+            Console.WriteLine(result.Person.Id);
+        }
+    }
+}
